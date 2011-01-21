078f5d3f2b92cb4ff40c8a69bce4671335a71c9b
Ryan Clark <ryan.clark.j@gmail.com>
2011-01-21 16:15:40 -0700

    Adjustment to urls.py that makes things a little smoother.

--- urls.py
+++ urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 from uriresolve.models import *
 
 urlpatterns = patterns('',
-    (r'^$', 'django.views.generic.simple.redirect_to', {'url': '/uri-gin/uri-description/'}),
+    (r'^$', 'django.views.generic.simple.redirect_to', {'url': 'uri-description/'}),
     (r'^(?P<authority_name>.+)/type/json$', 'uriresolve.views.jsonTypesByAuthority'),
     (r'^(?P<given_uri>.*)uri-description(?:/|\.html)$', 'uriresolve.views.description'),
     (r'^(?P<given_uri>.+)$', 'uriresolve.views.resolver'),
